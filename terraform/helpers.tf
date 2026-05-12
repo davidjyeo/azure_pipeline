adb0a4e3dd263f812a62d68a97a75fe8a70a888f
David Yeo <david.j.yeo@outlook.com>
2026-05-12 17:32:12 +0100

    working directory for tf plan

--- terraform/helpers.tf
+++ terraform/helpers.tf
@@ -4,13 +4,14 @@ module "naming" {
   suffix = [
     "az",
     "djy",
-    var.environment
+    var.environment,
+    "${module.avm-utl-regions.regions[0].short_name}"
   ]
 }
-
 
 module "avm-utl-regions" {
     version = "0.12.0"
     source  = "Azure/avm-utl-regions/azurerm"
-
+    geography_filter = var.region
+    enable_telemetry = var.enable_telemetry
 }
