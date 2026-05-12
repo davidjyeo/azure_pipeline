89501003c6bd818f668848a84b68b3f7a39325a1
David Yeo <david.j.yeo@outlook.com>
2026-05-12 16:49:04 +0100

    working directory for tf plan

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -2,3 +2,13 @@
 #   description = "The subscription ID where the infrastructure will be deployed."
 #   type        = string
 # }
+
+variable "region" {
+  description = "The Azure region where resources will be deployed."
+#   type        = string
+#   default     = "eastus"
+}
+
+variable "environment" {
+  description = "The deployment environment (e.g., dev, uat, prod)."  
+}
