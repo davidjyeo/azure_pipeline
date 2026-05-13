856919d7db242f270094121e9e87f051d3f6f66a
David Yeo <david.j.yeo@outlook.com>
2026-05-13 10:56:07 +0100

    Build(TF): subscriptions

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,3 +1,7 @@
 # output "region_short_name" {
 #   value = module.avm-utl-regions.regions.short_name
 # }
+
+output "subscriptions" {
+  value = data.azurerm_subscriptions.available.subscriptions
+}
